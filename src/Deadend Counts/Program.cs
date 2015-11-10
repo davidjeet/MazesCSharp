4b419c66df8bbe2f327e401b2f6df9e082a5b369
David Green <davidgreen24@gmail.com>
2015-11-09 21:24:57 -0500

    Deadcounts completed!

--- src/Deadend Counts/Program.cs
+++ src/Deadend Counts/Program.cs
@@ -12,14 +12,44 @@ namespace Deadend_Counts
     {
         static void Main(string[] args)
         {
-            ////IGrid grid = new Grid(10, 10);
-            //// BinaryTree.Run(ref grid);
+            var algorithms = new IAlgorithms[] { new BinaryTree(), new SideWinder(), new AldousBroder(), new HuntAndKill() };
 
-            ////Console.WriteLine(grid.ToString(true));
+            var tries = 100;
+            var size = 20;
 
-            ////var deadends = ((Grid)grid).DeadEnds;
-            ////Console.WriteLine($"There were {deadends.Count()} dead ends found.");
-            ////Console.ReadKey();
+            var averages = new Dictionary<string, double>();
+
+            foreach(var algorithm in algorithms)
+            {
+                var name = algorithm.GetType().Name;
+                Console.WriteLine($"Running {name} algorithm");
+                var deadendCounts = new List<int>();
+                for(int i=0;i<tries;i++)
+                {
+                    IGrid grid = new Grid(size, size);
+                    algorithm.Run(ref grid);
+                    deadendCounts.Add(((Grid)grid).DeadEnds.Count());                     
+                }
+
+                var totalDeadends = deadendCounts.Sum();
+                averages.Add(name, totalDeadends / deadendCounts.Count);
+            }
+
+
+            var totalCells = size * size;
+            Console.WriteLine(" ");
+            Console.WriteLine($"Average dead-ends per {size}x{size} maze ({totalCells} cells)");
+            Console.WriteLine(" ");
+
+            var sortedAlgorithm = averages.OrderByDescending(kvp => kvp.Value);
+            foreach(var algorithm in sortedAlgorithm)
+            {
+                var percentage = (algorithm.Value * 100.0) / (size * size);
+                Console.WriteLine($"{algorithm.Key}: {algorithm.Value}/{size} ({percentage}%)");
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
