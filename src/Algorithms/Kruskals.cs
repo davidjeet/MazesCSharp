0cb342487e0d33e8208222fa7debf8ec91a2570e
David Green <davidgreen24@gmail.com>
2015-11-14 13:03:15 -0500

    Got it! (Kruskal)

--- src/Algorithms/Kruskals.cs
+++ src/Algorithms/Kruskals.cs
@@ -11,9 +11,37 @@ namespace Infrastructure.Algorithms
 {
     public class Kruskals : IAlgorithms
     {
+        class CellPair : IEquatable<CellPair>
+        {
+            public Cell Left { get; private set; }
+            public Cell Right { get; private set; }
+
+            public CellPair(Cell left, Cell right)
+            {
+                Left = left;
+                Right = right;
+            }
+
+            public bool Equals(CellPair other)
+            {
+                if (other == null) return false;
+                return (Left == other.Left && Right == other.Right);
+            }
+
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as CellPair);
+            }
+
+            public override int GetHashCode()
+            {
+                return $"{Left.ToString()}_{Right.ToString()}".GetHashCode();
+            }
+        }
+
         class State
         {
-            internal List<Cell []> Neighbors { get; set; } 
+            internal List<CellPair> Neighbors { get; set; } 
             internal Dictionary<Cell, int> setForCell { get; private set; }
             internal Dictionary<int, List<Cell>> cellsInSet { get; private set; }
             private IGrid grid;
@@ -21,7 +49,7 @@ namespace Infrastructure.Algorithms
             public State(ref IGrid _grid)
             {
                 grid = _grid;
-                Neighbors = new List<Cell []>();
+                Neighbors = new List<CellPair>();
                 setForCell = new Dictionary<Cell, int>();
                 cellsInSet = new Dictionary<int, List<Cell>>();
 
@@ -33,11 +61,11 @@ namespace Infrastructure.Algorithms
 
                     if (cell.South !=null)
                     {
-                        Neighbors.Add(new Cell[] { cell, cell.South });
+                        Neighbors.Add(new CellPair(cell, cell.South));
                     }
                     if (cell.East != null)
                     {
-                        Neighbors.Add(new Cell[] { cell, cell.East });
+                        Neighbors.Add(new CellPair(cell, cell.East));
                     }
                 }
             }
@@ -71,13 +99,13 @@ namespace Infrastructure.Algorithms
         {
             var state = new State(ref grid);
             var _neighbors = state.Neighbors.Shuffle();     // Shuffle<Cell []>()
-            var neighbors = new Stack<Cell[]>(_neighbors);
+            var neighbors = new Stack<CellPair>(_neighbors);
             
             while (neighbors.Any())
             {
                 var pair = neighbors.Pop();
-                var left = pair[0];
-                var right = pair[1];
+                var left = pair.Left;
+                var right = pair.Right;
                 if (state.CanMerge(left, right))
                 {
                     state.Merge(left, right);
