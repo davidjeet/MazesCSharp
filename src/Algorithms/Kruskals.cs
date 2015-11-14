714a6d435da62ffe13cc829da32b54acbdb37a6d
David Green <davidgreen24@gmail.com>
2015-11-14 07:18:33 -0500

    Added Kruskals, but it probably doesn't work
    Too much weird Ruby syntax I'm not sure about, lol. Added a bunch of
    TODOs which need to be deciphered.

--- src/Algorithms/Kruskals.cs
+++ src/Algorithms/Kruskals.cs
@@ -13,28 +13,81 @@ namespace Infrastructure.Algorithms
     {
         class State
         {
-            internal List<Cell> Neighbors { get; set; }
-        }
+            internal Stack<Cell []> Neighbors { get; set; } // TODO: issue 1, what structure is this
+            internal Dictionary<Cell, int> setForCell { get; private set; }
+            internal Dictionary<int, List<Cell>> cellsInSet { get; private set; }
+            private IGrid grid;
 
+            public State(ref IGrid _grid)
+            {
+                grid = _grid;
+                Neighbors = new Stack<Cell []>();
+                setForCell = new Dictionary<Cell, int>();
+                cellsInSet = new Dictionary<int, List<Cell>>();
+
+                foreach(var cell in grid)
+                {
+                    int set = setForCell.Count;
+                    setForCell[cell] = set;
+                    cellsInSet[set] = new List<Cell> { cell };
+
+                    if (cell.South !=null)
+                    {
+                        // TODO: issue 2, what structure is this
+                        Neighbors.Push(new Cell[] { cell, cell.South });
+                    }
+                    if (cell.East != null)
+                    {
+                        // TODO: issue 3, what structure is this
+                        Neighbors.Push(new Cell[] { cell, cell.East });
+                    }
+
+                }
+            }
+
+            internal bool CanMerge(Cell left, Cell right)
+            {
+                return setForCell[left] != setForCell[right];
+            }
+
+            internal void Merge(Cell left, Cell right)
+            {
+                left.Link(right);
+
+                var winner = setForCell[left];
+                var loser = setForCell[right];
+                var losers = cellsInSet[loser];
+                losers.Add(right); // TODO: issue 4, poor ruby  interpretation?
+
+                foreach (var cell in losers)
+                {
+                    cellsInSet[winner].Add(cell);
+                    setForCell[cell] = winner;
+                }
+
+                cellsInSet.Remove(loser);
+            }
+
+        } //end class
 
         public void Run(ref IGrid grid)
         {
-            ////State s;
-            ////s.
+            var state = new State(ref grid);
+            var neighbors = state.Neighbors; //  Shuffle<Cell>();
+            // TODO: issue 5, can't just shuffle Tuples!!!!1 
 
-            foreach (var cell in grid.GetAllCells())
+            while (neighbors.Any())
             {
-                var neighbors = new List<Cell>();
-                if (cell.North != null) neighbors.Add(cell.North);
-                if (cell.East != null) neighbors.Add(cell.East);
-
-                int index = GetRandomNumber(0, neighbors.Count);
-                if (neighbors.Count > 0)
+                // TODO: issue 6, suddenly we have stack like behavior?????
+                var pair = neighbors.Pop();
+                var left = pair[0];
+                var right = pair[1];
+                if (state.CanMerge(left, right))
                 {
-                    Cell neighbor = neighbors[index];
-                    cell.Link(neighbor);
+                    state.Merge(left, right);
                 }
             }
+
         }
     }
 }
