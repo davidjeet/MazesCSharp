06836a9e255fa59beba0920b212bdbe8026be8e8
David Green <davidgreen24@gmail.com>
2015-11-14 13:50:22 -0500

    Simplified Prims
    Simplified Prims!!!!1

---
+++ src/Algorithms/SimplifiedPrim.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Core;
+using Infrastructure.Core.Interfaces;
+
+namespace Infrastructure.Algorithms
+{
+    public class SimplifiedPrim : IAlgorithms
+    {
+        public void Run(ref IGrid grid)
+        {
+            var startAt = grid.GetRandomCell;
+            Stack<Cell> active = new Stack<Cell>();
+            active.Push(startAt);
+            int iterations = 1;
+            while(active.Any())
+            {
+                iterations++;
+                var cell = active.Sample();
+                var availableNeighbors = cell.Neighbors.Where(x => x.Links.Count == 0);
+
+                if (availableNeighbors.Any())
+                {
+                    var neighbor = availableNeighbors.Sample();
+                    cell.Link(neighbor);
+                    active.Push(neighbor);
+                }
+                else
+                {
+                    var list = active.ToList();
+                    list.Remove(cell);
+                    active = new Stack<Cell>(list);
+                }
+            }
+            Console.WriteLine($"Iterations: {iterations}");
+        }
+    }
+}
---
+++ src/Prim Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Algorithms;
+using Infrastructure.Core;
+using Infrastructure.Core.Interfaces;
+
+
+namespace Prim_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGrid grid = new Grid(20, 20);
+            new SimplifiedPrim().Run(ref grid);
+
+            Console.WriteLine(grid.ToString(false));
+            ////Console.WriteLine(grid.ToDebug());
+            Console.ReadKey();
+        }
+    }
+}
