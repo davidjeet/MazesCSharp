331e8914700c2afa3ec8e0b9ec04cff2d7cab127
David Green <davidgreen24@gmail.com>
2015-11-16 07:02:59 -0500

    First attempt at RecursionDivision algo

--- src/Algorithms/RecursiveDivision.cs
+++ src/Algorithms/RecursiveDivision.cs
@@ -16,31 +16,64 @@ namespace Infrastructure.Algorithms
         {
             grid = _grid;
 
-            foreach(var cell in grid)
+            foreach (var cell in grid)
             {
-
+                foreach (var neighbor in cell.Neighbors)
+                {
+                    cell.Link(neighbor, false);
+                }
             }
 
-            ////var cell = grid.GetRandomCell;
-            ////var unvisited = grid.Size - 1;
+            Divide(0, 0, grid.Rows, grid.Columns);
 
-            ////while(unvisited > 0)
-            ////{
-            ////    // next three lines get a random neighbor
-            ////    var neighbors = cell.Neighbors;
-            ////    int index = GetRandomNumber(0, neighbors.Count);
-            ////    var neighbor = neighbors[index];
+        }
 
-            ////    var links = neighbor.Links;
-            ////    if (links.Count == 0)
-            ////    {
-            ////        cell.Link(neighbor);
-            ////        unvisited--; 
-            ////    }
+        private void Divide(int row, int column, int height, int width)
+        {
+            if (height <= 1 || width <= 1) return;
 
-            ////    cell = neighbor;
-            ////}
+            if (height > width)
+                DivideHorizontally(row, column, height, width);
+            else
+                DivideVertically(row, column, height, width);
+        }
 
+
+
+        private void DivideHorizontally(int row, int column, int height, int width)
+        {
+            var divideSouthOf = GetRandomNumber(height - 1);
+            var passageAt = GetRandomNumber(width);
+
+            for (int x = 0; x < width; x++)
+            {
+                if (passageAt != x)
+                {
+                    var cell = grid[row + divideSouthOf, column + x];
+                    grid[row + divideSouthOf, column + x].UnLink(cell.South);
+                }
+            }
+
+            Divide(row, column, divideSouthOf + 1, width);
+            Divide(row + divideSouthOf + 1, column, height - divideSouthOf - 1, width);
+        }
+
+        private void DivideVertically(int row, int column, int height, int width)
+        {
+            var divideEastOf = GetRandomNumber(width - 1);
+            var passageAt = GetRandomNumber(height);
+
+            for (int y = 0; y < height; y++)
+            {
+                if (passageAt != y)
+                {
+                    var cell = grid[row + y, column + divideEastOf];
+                    grid[row + y, column + divideEastOf].UnLink(cell.East);
+                }
+            }
+
+            Divide(row, column, height, divideEastOf + 1);
+            Divide(row, column + divideEastOf + 1, height, width - divideEastOf - 1);
         }
     }
 }
