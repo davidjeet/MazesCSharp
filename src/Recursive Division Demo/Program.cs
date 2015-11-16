0ac01e120447d1d653c122ad279e08e962680a2f
David Green <davidgreen24@gmail.com>
2015-11-16 07:10:06 -0500

    Added Recursive Division Demo project

---
+++ src/Recursive Division Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Algorithms;
+using Infrastructure.Core;
+using Infrastructure.Core.Interfaces;
+using static Infrastructure.Core.Helper;
+
+namespace Recursive_Division_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGrid grid = new Grid(20, 20);
+            new RecursiveDivision().Run(ref grid);
+            Console.WriteLine(grid.ToString(false));
+            Console.ReadKey();
+        }
+    }
+}
